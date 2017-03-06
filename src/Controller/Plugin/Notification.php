c0af9ff777ebd1619aee799ce4384af86b1ae3ad
Szymon Michalowski <szmnmichalowski@gmail.com>
2017-03-06 21:11:44 +0100

    Add accessors for session manager and session container

--- src/Controller/Plugin/Notification.php
+++ src/Controller/Plugin/Notification.php
@@ -3,7 +3,56 @@
 namespace SzmNotification\Controller\Plugin;
 
 use Zend\Mvc\Controller\Plugin\AbstractPlugin;
+use Zend\Session\ManagerInterface as Manager;
+use Zend\Session\Container;
 
 class Notification extends AbstractPlugin
 {
+    /**
+     * @var Container
+     */
+    protected $container;
+
+    /**
+     * @var Manager
+     */
+    protected $sessionManager;
+
+    /**
+     * @param Manager $manager
+     * @return $this
+     */
+    public function setSessionManager(Manager $manager)
+    {
+        $this->sessionManager = $manager;
+
+        return $this;
+    }
+
+    /**
+     * @return Manager
+     */
+    public function getSessionManager()
+    {
+        if (!$this->sessionManager instanceof Manager) {
+            $this->setSessionManager(Container::getDefaultManager());
+        }
+
+        return $this->sessionManager;
+    }
+
+    /**
+     * @return Container
+     */
+    public function getContainer()
+    {
+        if ($this->container instanceof Container) {
+            return $this->container;
+        }
+
+        $manager = $this->getSessionManager();
+        $this->container = new Container('notifications', $manager);
+
+        return $this->container;
+    }
 }
--- test/Controller/Plugin/NotificationTest.php
+++ test/Controller/Plugin/NotificationTest.php
@@ -4,6 +4,8 @@ namespace SzmNotificationTest\Controller\Plugin;
 
 use PHPUnit\Framework\TestCase;
 use SzmNotification\Controller\Plugin\Notification;
+use Zend\Session\ManagerInterface as Manager;
+use Zend\Session\Container;
 
 class NotificationTest extends TestCase
 {
@@ -21,6 +23,23 @@ class NotificationTest extends TestCase
     public function testPluginExists()
     {
         $this->assertTrue(is_object($this->notification));
+    }
+
+    /**
+     * @covers SzmNotification\Controller\Plugin\Noty::setSessionManager
+     * @covers SzmNotification\Controller\Plugin\Noty::getSessionManager
+     */
+    public function testSessionManager()
+    {
+        $this->assertInstanceOf(Manager::class, $this->notification->getSessionManager());
+    }
+
+    /**
+     * @covers SzmNotification\Controller\Plugin\Noty::getContainer
+     */
+    public function testSessionContainer()
+    {
+        $this->assertInstanceOf(Container::class, $this->notification->getContainer());
     }
 }
 
