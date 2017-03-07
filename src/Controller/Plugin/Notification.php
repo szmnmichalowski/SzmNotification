7b3a961b1c665a3bc372960488500b5fd7c954c0
Szymon Michalowski <szmnmichalowski@gmail.com>
2017-03-07 21:48:22 +0100

    Add 'add', 'get', 'has' methods

--- src/Controller/Plugin/Notification.php
+++ src/Controller/Plugin/Notification.php
@@ -5,6 +5,7 @@ namespace SzmNotification\Controller\Plugin;
 use Zend\Mvc\Controller\Plugin\AbstractPlugin;
 use Zend\Session\ManagerInterface as Manager;
 use Zend\Session\Container;
+use Zend\Stdlib\SplQueue;
 
 class Notification extends AbstractPlugin
 {
@@ -44,6 +45,11 @@ class Notification extends AbstractPlugin
     protected $notifications = [];
 
     /**
+     * @var bool
+     */
+    protected $isAdded = false;
+
+    /**
      * @param Manager $manager
      * @return $this
      */
@@ -79,6 +85,69 @@ class Notification extends AbstractPlugin
         $this->container = new Container('notifications', $manager);
 
         return $this->container;
+    }
+
+    /**
+     * @param $type
+     * @param $message
+     * @param int $hops
+     * @return $this
+     */
+    public function add($type, $message, $hops = 1)
+    {
+        $container = $this->getContainer();
+
+        if (!$this->isAdded) {
+            $this->getNotificationsFromContainer();
+            $container->setExpirationHops($hops);
+        }
+
+        if (!isset($container->{$type}) || !$container->{$type} instanceof SplQueue) {
+            $container->{$type} = new SplQueue();
+        }
+
+        $container->{$type}->push($message);
+        $this->isAdded = true;
+
+        return $this;
+    }
+
+    /**
+     * @param $type
+     * @param $message
+     * @return $this
+     */
+    public function __invoke($type = null, $message = null)
+    {
+        if (!$type || !$message) {
+            return $this;
+        }
+
+        return $this->add($type, $message);
+    }
+
+    /**
+     * @param $namespace
+     * @return array
+     */
+    public function get($namespace)
+    {
+        if ($this->has($namespace)) {
+            return $this->notifications[$namespace]->toArray();
+        }
+
+        return [];
+    }
+
+    /**
+     * @param $namespace
+     * @return bool
+     */
+    public function has($namespace)
+    {
+        $this->getNotificationsFromContainer();
+
+        return isset($this->notifications[$namespace]);
     }
 
     /**
--- test/Controller/Plugin/NotificationTest.php
+++ test/Controller/Plugin/NotificationTest.php
@@ -26,8 +26,8 @@ class NotificationTest extends TestCase
     }
 
     /**
-     * @covers SzmNotification\Controller\Plugin\Noty::setSessionManager
-     * @covers SzmNotification\Controller\Plugin\Noty::getSessionManager
+     * @covers SzmNotification\Controller\Plugin\Notification::setSessionManager
+     * @covers SzmNotification\Controller\Plugin\Notification::getSessionManager
      */
     public function testSessionManager()
     {
@@ -35,7 +35,7 @@ class NotificationTest extends TestCase
     }
 
     /**
-     * @covers SzmNotification\Controller\Plugin\Noty::getContainer
+     * @covers SzmNotification\Controller\Plugin\Notification::getContainer
      */
     public function testSessionContainer()
     {
@@ -43,8 +43,8 @@ class NotificationTest extends TestCase
     }
 
     /**
-     * @covers SzmNotification\Controller\Plugin\Noty::getNotificationsFromContainer
-     * @covers SzmNotification\Controller\Plugin\Noty::clearNotificationsFromContainer
+     * @covers SzmNotification\Controller\Plugin\Notification::getNotificationsFromContainer
+     * @covers SzmNotification\Controller\Plugin\Notification::clearNotificationsFromContainer
      */
     public function testClearContainer()
     {
@@ -59,6 +59,77 @@ class NotificationTest extends TestCase
         $sessionNotifications = $container->{$type};
 
         $this->assertTrue(empty($sessionNotifications));
+    }
+
+    /**
+     * @covers SzmNotification\Controller\Plugin\Notification::has
+     */
+    public function testHasNotifications()
+    {
+        $type = 'info';
+        $text = 'foo bar';
+
+        $this->notification->add($type, $text);
+        $plugin = new Notification();
+
+        $this->assertTrue($plugin->has($type));
+    }
+
+    /**
+     * @covers SzmNotification\Controller\Plugin\Notification::get
+     */
+    public function testGetNotifications()
+    {
+        $type = 'info';
+        $message = 'foo bar';
+
+        $this->notification->add($type, $message);
+        $plugin = new Notification();
+
+        $this->assertEquals([$message], $plugin->get($type));
+    }
+
+    /**
+     * @covers SzmNotification\Controller\Plugin\Notification::__invoke
+     */
+    public function testInvokeMethod()
+    {
+        $type = 'info';
+        $message = 'foo bar';
+
+        $this->notification->__invoke($type, $message);
+        $this->assertEquals([$message], $this->notification->getCurrent($type));
+    }
+
+    /**
+     * @covers SzmNotification\Controller\Plugin\Notification::add
+     */
+    public function testAddNotification()
+    {
+        $type = 'info';
+        $message = 'foo bar';
+
+        $this->notification->add($type, $message);
+        $plugin = new Notification();
+
+        $this->assertEquals([$message], $plugin->get($type));
+    }
+
+    /**
+     * @covers SzmNotification\Controller\Plugin\Notification::add
+     */
+    public function testMultipleNotifications()
+    {
+        $type = 'info';
+        $message = 'foo bar1';
+        $message2 = 'foo bar2';
+
+        $this->notification->add($type, $message);
+        $this->notification->add($type, $message2);
+
+        $plugin = new Notification();
+
+        $this->assertEquals([$message, $message2], $plugin->get($type));
     }
 }
 
